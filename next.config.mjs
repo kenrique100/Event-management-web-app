2e7c027253e16a5fab1d8cb3d041827565f641a6
Kenrique _Ngwa <72971484+kenrique100@users.noreply.github.com>
2024-07-27 20:06:47 -0400

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,18 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'utfs.io',
-          port: ''
-        }
-      ]
-    },
-    productionBrowserSourceMaps: true,  // Enable source maps in production
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        port: '',
+      },
+    ],
+  },
+  productionBrowserSourceMaps: true, // Enable source maps in production
+};
+
+// Export default using ES module syntax
+export default nextConfig;
